1e21a46b14b893e1ad085602e188f6b1cecb5060
Roanak Baviskar <roanakb@Roanaks-MacBook-Air.local>
2024-10-21 14:12:54 -0700

    added comments

--- run_inference_test.sh
+++ run_inference_test.sh
@@ -1,12 +1,17 @@
+# confirm correct command line args are passed
 echo "Your hfcli token is $1"
 echo "The model you are testing is $2"
 
+# clone repo
 git clone https://github.com/roanakb/llama-recipes.git
 
+# install pip
 apt install python3-pip -y
 
+# install requirements
 pip install -r llama-recipes/tools/benchmarks/inference/requirements.txt
 
+# login hf console for gated models
 huggingface-cli login --token $1
 
 # results will be printed to console and outputted to csv files
